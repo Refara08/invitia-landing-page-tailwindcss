dedf8493c604d0b0d8d7c6ecaada257861930e0e
Gema Refantero <refara.08@gmail.com>
2022-04-09 13:14:48 +0700

    fixed mobile nav open-close animation

--- public/app.js
+++ public/app.js
@@ -6,47 +6,75 @@ const logoTextRight = document.querySelector("#logo-text-right");
 
 burger.addEventListener("click", () => {
   console.log("hello");
+  // BUKA burger menu -------------------------------------------------------//
   if (mobileMenu.classList.contains("hidden")) {
+    // display block mobile menu
     mobileMenu.classList.remove("hidden");
 
-    nav.classList.remove("bg-white");
-    nav.classList.add("bg-black");
-    burger.classList.add("hamburger-active");
-    logoTextLeft.classList.remove("translate-x-10");
-    logoTextRight.classList.add("-translate-x-10");
+    // ----- jeda untuk display block mobile menu -----
 
     setTimeout(() => {
+      // nav
+      nav.classList.remove("bg-white");
+      nav.classList.add("bg-black");
+
+      // logo remove attr from unused link
       logoTextLeft.setAttribute("href", "#");
       logoTextRight.removeAttribute("href");
+      // logo transition
+      logoTextLeft.classList.remove("translate-x-10");
+      logoTextRight.classList.remove("-translate-x-0");
+      logoTextLeft.classList.add("translate-x-0");
+      logoTextRight.classList.add("-translate-x-10");
+
+      // burger menu animation
+      burger.classList.add("hamburger-active");
     }, 25);
 
-    setTimeout(() => {
-      mobileMenu.classList.remove("-translate-y-1");
-      mobileMenu.classList.remove("opacity-0");
-      mobileMenu.classList.remove("bg-white");
-      mobileMenu.classList.add("-translate-y-0");
-      mobileMenu.classList.add("opacity-100");
-      mobileMenu.classList.add("bg-black");
-    }, 100);
-  } else {
-    mobileMenu.classList.remove("-translate-y-0");
-    mobileMenu.classList.remove("opacity-100");
-    mobileMenu.classList.remove("bg-black");
-    mobileMenu.classList.add("opacity-0");
-    mobileMenu.classList.add("-translate-y-1");
-    mobileMenu.classList.add("bg-white");
+    // -------- jeda 300 ms -----------
 
-    // setTimeout(() => {}, 225);
     setTimeout(() => {
-      logoTextRight.setAttribute("href", "#");
-      logoTextLeft.removeAttribute("href");
+      // mobile menu transition
+      mobileMenu.classList.remove("opacity-0");
+      mobileMenu.classList.remove("-translate-y-5");
+      mobileMenu.classList.add("-translate-y-0");
+    }, 325);
+
+    // TUTUP burger menu -------------------------------------------------------//
+  } else {
+    setTimeout(() => {
+      // mobile menu transition
+      mobileMenu.classList.remove("-translate-y-0");
+      mobileMenu.classList.add("-translate-y-5");
+      mobileMenu.classList.add("opacity-0");
+    }, 25);
+
+    // -------- jeda 300 ms -----------
+
+    setTimeout(() => {
+      // nav
       nav.classList.remove("bg-black");
       nav.classList.add("bg-white");
-      burger.classList.remove("hamburger-active");
+
+      // logo remove attr
+      logoTextRight.setAttribute("href", "#");
+      logoTextLeft.removeAttribute("href");
+
+      // logo transition
+      logoTextLeft.classList.remove("translate-x-0");
       logoTextLeft.classList.add("translate-x-10");
       logoTextRight.classList.remove("-translate-x-10");
+      logoTextRight.classList.add("-translate-x-0");
 
+      // burger menu animation
+      burger.classList.remove("hamburger-active");
+    }, 325);
+
+    // ---- jeda 150 ms untuk display none mobile menu --------
+
+    setTimeout(() => {
+      // mobile menu display none
       mobileMenu.classList.add("hidden");
-    }, 100);
+    }, 200);
   }
 });
