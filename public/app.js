eed2911971217dd342da1b487007d960aae9c8fa
Gema Refantero <refara.08@gmail.com>
2022-04-10 14:58:08 +0700

    added scroll event for nav, also adjusted it transitions

--- public/app.js
+++ public/app.js
@@ -1,80 +1,118 @@
+// ### Variables ### =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=--
 const burger = document.querySelector("#hamburger");
+const burgerLineArr = burger.children;
 const mobileMenu = document.querySelector("#menu");
 const nav = document.querySelector("#nav");
 const logoTextLeft = document.querySelector("#logo-text-left");
 const logoTextRight = document.querySelector("#logo-text-right");
 
+// ### Functions ### =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=--
+const navBlack = () => {
+  // nav
+  nav.classList.remove("bg-white");
+  nav.classList.add("bg-black");
+
+  // logo remove attr from unused link
+  logoTextLeft.setAttribute("href", "#");
+  logoTextRight.removeAttribute("href");
+  // logo transition
+  logoTextLeft.classList.remove("translate-x-10");
+  logoTextRight.classList.remove("-translate-x-0");
+  logoTextLeft.classList.add("translate-x-0");
+  logoTextRight.classList.add("-translate-x-10");
+};
+
+const navWhite = () => {
+  // nav
+  nav.classList.remove("bg-black");
+  nav.classList.add("bg-white");
+
+  // logo remove attr
+  logoTextRight.setAttribute("href", "#");
+  logoTextLeft.removeAttribute("href");
+
+  // logo transition
+  logoTextLeft.classList.remove("translate-x-0");
+  logoTextLeft.classList.add("translate-x-10");
+  logoTextRight.classList.remove("-translate-x-10");
+  logoTextRight.classList.add("-translate-x-0");
+};
+
+const mobileActive = () => {
+  // mobile menu transition
+  mobileMenu.classList.remove("opacity-0");
+  mobileMenu.classList.remove("-translate-y-5");
+  mobileMenu.classList.add("-translate-y-0");
+};
+
+const mobileDeactive = () => {
+  // mobile menu transition
+  mobileMenu.classList.remove("-translate-y-0");
+  mobileMenu.classList.add("-translate-y-5");
+  mobileMenu.classList.add("opacity-0");
+};
+
+// ### Events ### =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=---=-=-=
+
+// 1 - hamburger click event // =============================================================
 burger.addEventListener("click", () => {
   console.log("hello");
   // BUKA burger menu -------------------------------------------------------//
   if (mobileMenu.classList.contains("hidden")) {
-    // display block mobile menu
     mobileMenu.classList.remove("hidden");
 
-    // ----- jeda untuk display block mobile menu -----
-
     setTimeout(() => {
-      // nav
-      nav.classList.remove("bg-white");
-      nav.classList.add("bg-black");
-
-      // logo remove attr from unused link
-      logoTextLeft.setAttribute("href", "#");
-      logoTextRight.removeAttribute("href");
-      // logo transition
-      logoTextLeft.classList.remove("translate-x-10");
-      logoTextRight.classList.remove("-translate-x-0");
-      logoTextLeft.classList.add("translate-x-0");
-      logoTextRight.classList.add("-translate-x-10");
+      navBlack();
 
       // burger menu animation
       burger.classList.add("hamburger-active");
     }, 25);
 
-    // -------- jeda 300 ms -----------
-
     setTimeout(() => {
-      // mobile menu transition
-      mobileMenu.classList.remove("opacity-0");
-      mobileMenu.classList.remove("-translate-y-5");
-      mobileMenu.classList.add("-translate-y-0");
+      // mobile nav active
+      mobileActive();
     }, 325);
 
     // TUTUP burger menu -------------------------------------------------------//
   } else {
     setTimeout(() => {
-      // mobile menu transition
-      mobileMenu.classList.remove("-translate-y-0");
-      mobileMenu.classList.add("-translate-y-5");
-      mobileMenu.classList.add("opacity-0");
+      mobileDeactive();
     }, 25);
 
-    // -------- jeda 300 ms -----------
-
     setTimeout(() => {
-      // nav
-      nav.classList.remove("bg-black");
-      nav.classList.add("bg-white");
-
-      // logo remove attr
-      logoTextRight.setAttribute("href", "#");
-      logoTextLeft.removeAttribute("href");
-
-      // logo transition
-      logoTextLeft.classList.remove("translate-x-0");
-      logoTextLeft.classList.add("translate-x-10");
-      logoTextRight.classList.remove("-translate-x-10");
-      logoTextRight.classList.add("-translate-x-0");
+      if (window.scrollY > 100) {
+        navBlack();
+      } else {
+        navWhite();
+      }
 
       // burger menu animation
       burger.classList.remove("hamburger-active");
     }, 325);
 
-    // ---- jeda 150 ms untuk display none mobile menu --------
-
     setTimeout(() => {
       // mobile menu display none
       mobileMenu.classList.add("hidden");
     }, 200);
+  }
+});
+
+// 2 - nav scroll bg-color change // =============================================================
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    // console.log("hitam");
+    navBlack();
+
+    Array.from(burgerLineArr).forEach((line) => {
+      line.classList.remove("bg-black");
+      line.classList.add("bg-white");
+    });
+  } else {
+    // console.log("putih");
+    navWhite();
+    Array.from(burgerLineArr).forEach((line) => {
+      line.classList.remove("bg-white");
+      line.classList.add("bg-black");
+    });
   }
 });
